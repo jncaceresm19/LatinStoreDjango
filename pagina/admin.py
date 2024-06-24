a9936c6f3cb0f2cab2198595ebaeb48cddf4082d
jncaceresm19 <139356958+jncaceresm19@users.noreply.github.com>
2024-06-23 21:21:41 -0400

    Correcion BD

--- pagina/admin.py
+++ pagina/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Marca, Cliente, Pedido
 
 # Register your models here.
 admin.site.register(Producto)
+admin.site.register(Marca)
+admin.site.register(Cliente)
+admin.site.register(Pedido)
--- pagina/models.py
+++ pagina/models.py
@@ -1,11 +1,37 @@
 from django.db import models
 
+class Marca(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+
 class Producto(models.Model):
     id_producto = models.CharField(max_length=5, primary_key=True)
     nombre_producto = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)  
     stock = models.IntegerField(default=0)
-    precio = models.IntegerField(default=0)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)  
     fecha_recepcion = models.DateField()
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    tallas_disponibles = models.CharField(max_length=100, blank=True, null=True)  
 
     def __str__(self):
         return self.nombre_producto
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    direccion = models.TextField()
+    
+    def __str__(self):
+        return self.nombre
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    fecha_pedido = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Pedido {self.id} de {self.cliente.nombre}"
